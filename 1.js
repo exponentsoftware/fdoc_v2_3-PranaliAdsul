f71216542c475a633054d79c86d6eb6c5c2d6922
PranaliAdsul <pranuadsulst1137@gmail.com>
2023-05-15 22:42:24 +0530

    Adding More Companies

---
+++ 1.js
@@ -0,0 +1,28 @@
+const companies = [
+    {
+      name: 'Company 1',
+      employees: [
+        { name: 'Alice', role: 'Developer', department: 'Engineering' },
+        { name: 'Bob', role: 'Manager', department: 'Engineering' },
+        { name: 'Charlie', role: 'Designer', department: 'Design' },
+      ],
+    },
+     {
+      name: 'Company 2',
+      employees: [
+        { name: 'Anna', role: 'Software Engineer', department: 'Engineering' },
+        { name: 'Joy', role: 'Ass Manager', department: 'Engineering' },
+        { name: 'Manu', role: 'fullstack', department: 'Frontend Developer' },
+        { name: 'Urvi', role: 'UI Designer', department: 'Frontend Developer' },
+      ],
+    },
+    {
+      name: 'Company 3',
+      employees: [
+        { name: 'Aman', role: 'Team Leader', department: 'Assistant Manager' },
+        { name: 'Pratik', role: 'Senior Consultant', department: 'Consultant' },
+      ],
+    },
+    // ... (more companies)
+  ];
+  
