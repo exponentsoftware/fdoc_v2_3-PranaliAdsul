dac3b0b9e1120cd65fc9e74e92c5704b1696b364
PranaliAdsul <pranuadsulst1137@gmail.com>
2023-05-17 17:54:30 +0530

    array of the project with information is added

---
+++ 2.js
@@ -0,0 +1,38 @@
+const projects = [
+    {
+      name: "ImpactX",
+      client: "Nova Enterprises",
+      tasks: [
+        {
+          title: "Event Planning and Coordination",
+          assignee: "John",
+          status: "In Progress",
+          dueDate: "2023-05-15",
+        },
+        {
+          title: "Branding and Identity Development",
+          assignee: "Alice",
+          status: "Completed",
+          dueDate: "2023-05-10",
+        },
+      ],
+    },
+    {
+      name: "CatalystX",
+      client: "Apex Industries",
+      tasks: [
+        {
+          title: "Continuous Improvement Programs and Lean Manufacturing",
+          assignee: "Bob",
+          status: "In Progress",
+          dueDate: "2023-05-20",
+        },
+        {
+          title: "Employee Training and Development ",
+          assignee: "Alice",
+          status: "Not Started",
+          dueDate: "2023-05-18",
+        },
+      ],
+    },
+  ];
