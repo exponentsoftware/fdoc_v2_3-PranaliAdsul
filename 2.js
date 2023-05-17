6ce98c5c8751d3c7495ac79d24b9c64bd816bea3
PranaliAdsul <pranuadsulst1137@gmail.com>
2023-05-17 18:04:27 +0530

    find Over due Task Solved

--- 2.js
+++ 2.js
@@ -7,13 +7,13 @@ const projects = [
           title: "Event Planning and Coordination",
           assignee: "John",
           status: "In Progress",
-          dueDate: "2023-05-15",
+          dueDate: "2023-04-15",
         },
         {
           title: "Branding and Identity Development",
           assignee: "Alice",
           status: "Completed",
-          dueDate: "2023-05-10",
+          dueDate: "2023-04-10",
         },
       ],
     },
@@ -25,14 +25,36 @@ const projects = [
           title: "Continuous Improvement Programs and Lean Manufacturing",
           assignee: "Bob",
           status: "In Progress",
-          dueDate: "2023-05-20",
+          dueDate: "2023-04-20",
         },
         {
           title: "Employee Training and Development ",
-          assignee: "Alice",
+          assignee: "Tina",
           status: "Not Started",
-          dueDate: "2023-05-18",
+          dueDate: "2023-04-18",
         },
       ],
     },
   ];
+  // a. `findOverdueTasks` - Returns an array of tasks with a due date before a specified date.
+function findOverdueTasks(projects, specifiedDate) {
+    const overdueTasks = [];
+  
+     for (let i = 0; i < projects.length; i++) {
+       const project = projects[i];
+  
+      for (let j = 0; j < project.tasks.length; j++) {
+        const task = project.tasks[j];
+        if (task.dueDate < specifiedDate) {
+          overdueTasks.push(task);
+        }
+      }
+    }
+  
+    return overdueTasks;
+  }
+  
+  const specifiedDate = "2023-05-01";
+  const overdueTasks = findOverdueTasks(projects, specifiedDate);
+  console.log(overdueTasks);
+  
